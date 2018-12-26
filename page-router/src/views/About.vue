a8e851f95ad6bee808c330406b926fe889bda536
Stephanie <stephanie@wabow.com>
2018-12-26 19:29:22 +0800

    bind class

--- page-router/src/views/About.vue
+++ page-router/src/views/About.vue
@@ -3,9 +3,23 @@
       <VueLogoImage />
       <test msg="This is an test page"/>
       <about msg="This is an about page"/>
+      <p>
+        <button @click="pop">Click me!</button>
+      </p>
+      <div :class="{ show: isActive }" class="hide">ADDTOCART<br>ADDTOCART<br>ADDTOCART<br>ADDTOCART<br>ADDTOCART</div>
     
   </div>
 </template>
+
+<style>
+.hide {
+  display: none;
+}
+.show {
+  display: block;
+}
+</style>
+
 
 <script>
 import VueLogoImage from '@/components/VueLogoImage.vue'
@@ -15,6 +29,16 @@ import about from '@/components/about.vue'
 
 export default {
   name: 'home',
+  data: function () {
+    return {
+      isActive: false,
+    }
+  },
+  methods: {
+    pop: function() {
+      this.isActive = true
+    }
+  },
   components: {
     VueLogoImage,
     test,
